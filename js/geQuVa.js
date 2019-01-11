43ae006acdb4698e612c5118d5e6317adb7c439e
arachi007 <rr1047269@swccd.edu>
2019-01-11 23:14:43 +0700

    Create geQuVa.js

---
+++ js/geQuVa.js
@@ -0,0 +1,5 @@
+//<![CDATA[ 
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('b c(5){2 6=7.d.e.f(1).g("&m=1","");2 8=h(j(7.k(6)));2 3=8.9("&");l(2 i=0;i<3.n;i++){2 4=3[i].9("=");o(4[0]==5){a 4[1]}}a(p)}',26,26,'||var|vars|pair|variable|query|window|dec|split|return|function|getQueryVariable|location|search|substring|replace|decodeURIComponent||escape|atob|for||length|if|false'.split('|'),0,{}))
+
+//]]>
