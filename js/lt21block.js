231d00ee8a00080df30c31de1bfece267e6aa5dc
arachi007 <rr1047269@swccd.edu>
2019-03-24 19:20:51 +0700

    Update lt21block.js

--- js/lt21block.js
+++ js/lt21block.js
@@ -1,7 +1,9 @@
-!function(){function f(){
+!function(){
+  function f(){
 var a='<div class="alert alert-danger" style="display: inline-block;">Please disable Adblock to proceed to the destination page.</div>';
 $('.wpsafe-top').replaceWith(a);
 $('.wpsafe-bottom').replaceWith(a);
   $('.btn-xyz').replaceWith(a);
-  
-}();
+};
+var b=document.createElement("script");b.type="text/javascript";
+b.async=!0;b.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}()
