57817d04684d25102f7d61a2bf3d87410a744dbc
arachi007 <rr1047269@swccd.edu>
2019-03-24 19:13:56 +0700

    Create lt21block.js

---
+++ js/lt21block.js
@@ -0,0 +1,5 @@
+!function(){function f(){
+var a='<div class="alert alert-danger" style="display: inline-block;">Please disable Adblock to proceed to the destination page.</div>';
+$('.wpsafe-top').replaceWith(a);
+$('.wpsafe-bottom').replaceWith(a);
+}();
