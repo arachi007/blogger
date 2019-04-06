ac02fe58b3e270bdbfbd4bc10d533a5a406cccfa
arachi007 <rr1047269@swccd.edu>
2019-04-06 16:01:06 +0700

    Update verifblock.js

--- js/verifblock.js
+++ js/verifblock.js
@@ -1,7 +1,8 @@
 !function(){
   function f(){
 var a='<div class="alert alert-danger">Please disable Adblock to proceed to the destination page.</div>';
-document.getElementId("btn-xyz").innerHTML = a;
+var secondbtn = document.getElementById("btn-xyz");
+secondbtn.parentNode.replaceChild(a,secondbtn);
 };
 var b=document.createElement("script");b.type="text/javascript";
 b.async=!0;b.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}()
